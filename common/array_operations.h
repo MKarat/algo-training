6e6aea8db25d4585141c531aed5d06091a4d4abf
Maksim Karatunov <karatunov@rudn.ru>
2023-12-06 18:25:30 +0300

    refactor: transfer MergeSorted(...) to the common/

--- common/array_operations.h
+++ common/array_operations.h
@@ -8,4 +8,47 @@ void Swap(double* ptr1, double* ptr2){
     *ptr2 = buf;
 }
 
+
+// функция слияния двух неубывающих последовательностей
+int MergeSorted (double** c, const double* a, const double* b, 
+                    unsigned int size_a, unsigned int size_b){
+
+    unsigned int size_c = size_a + size_b; // размер объединенного массива
+
+    *c = (double*)malloc(size_c * sizeof(double)); // выделение требуемой памяти
+
+    int i = 0; // итератор для массива а
+    int j = 0; // итератор для массива b
+    
+    // цикл заполнения массива c
+    for (unsigned int k = 0; k < size_c; ++k){
+        
+        // контроль достижения конца массива а
+        if (i+1 > size_a){
+            (*c)[k] = b[j]; // оставшиеся элементы берём из массива b
+            ++j;
+            continue;
+        }
+
+        // контроль достижения конца массива b
+        if (j+1 > size_b){
+            (*c)[k] = a[i]; // оставшиеся элементы берём из массива a
+            ++i;
+            continue;
+        }
+
+        // выбор минимального элемента из текущих a[i] и b[j] в качестве
+        // очередного элемента для массива c
+        if (a[i] < b[j]){
+            (*c)[k] = a[i];
+            ++i;
+        } else {
+            (*c)[k] = b[j];
+            ++j;
+        }        
+    }
+    
+    return size_c;
+}
+
 #endif
